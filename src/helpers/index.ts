c04ca89ce88d3c8a27eafc673d7a757067e22090
Alex Sorokin <collagememorycollage@gmail.com>
2023-11-13 13:16:20 -0800

    добавили логгирование в отдельный файл rest.log

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,2 +1,3 @@
 export * from './common.js';
 export * from '../shared/libs/file-reader/offer.js';
+export * from './file-system.js';
--- src/shared/libs/logger/pino.logger.ts
+++ src/shared/libs/logger/pino.logger.ts
@@ -1,11 +1,22 @@
-import { Logger as PinoInstance, pino } from 'pino';
+import { Logger as PinoInstance, pino, transport } from 'pino';
 import { Logger } from './logger.interface.js';
+import { getCurrentModuleDerectory } from '../../../helpers/file-system.js';
+import {resolve} from 'node:path';
 
 export class PinoLogger implements Logger {
   private readonly logger: PinoInstance;
 
   constructor() {
-    this.logger = pino();
+    const modulePath = getCurrentModuleDerectory();
+    const logFilePath = './2189499-six-cities-5/log/rest.log';
+    const destination = resolve(modulePath, '../../../', logFilePath);
+
+    const fileTransport = transport({
+      target: 'pino/file',
+      options: {destination}
+    });
+
+    this.logger = pino({}, fileTransport);
   }
 
   public debug(message: string, ...args: unknown[]): void {
